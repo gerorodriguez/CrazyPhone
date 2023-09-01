9306f8104aa29ee2c4a01c689f3c56b8c6096ea6
gerorodriguez <gero0808yt@gmail.com>
2023-09-01 18:44:00 -0300

    Initial commit

---
+++ src/main/java/com/crazyphone/CrazyPhone/CrazyPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.crazyphone.CrazyPhone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyPhoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crazyphone/CrazyPhone/CrazyPhoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazyphone.CrazyPhone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyPhoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
