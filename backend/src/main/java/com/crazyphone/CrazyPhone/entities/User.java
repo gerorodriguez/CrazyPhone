f33747a42438987386c6993aefe0b45bdf1512af
gerorodriguez <110501756+gerorodriguez@users.noreply.github.com>
2023-09-05 00:14:59 -0300

    add user entity

---
+++ backend/src/main/java/com/crazyphone/CrazyPhone/entities/User.java
@@ -0,0 +1,59 @@
+package com.crazyphone.CrazyPhone.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cp_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long Id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
