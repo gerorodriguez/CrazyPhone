21fe1321fafb8a98ca5fd10482fc13601057c6a7
Manuel Gainza <gainza2014@gmail.com>
2023-09-20 18:24:53 -0300

    Add get all Publications endpoint

--- backend/src/main/java/com/crazyphone/CrazyPhone/controllers/PublicationController.java
+++ backend/src/main/java/com/crazyphone/CrazyPhone/controllers/PublicationController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/publications")
 public class PublicationController {
@@ -26,6 +28,11 @@ public class PublicationController {
     @PostMapping
     public ResponseEntity<PublicationDTO> save(@RequestBody PublicationDTO publicationDTO) {
         return new ResponseEntity<>(publicationService.save(publicationDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PublicationDTO>> getAll() {
+        return new ResponseEntity<>(publicationService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
--- backend/src/main/java/com/crazyphone/CrazyPhone/services/PublicationService.java
+++ backend/src/main/java/com/crazyphone/CrazyPhone/services/PublicationService.java
@@ -7,6 +7,8 @@ import com.crazyphone.CrazyPhone.services.mapper.PublicationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PublicationService {
     private final PublicationRepository publicationRepository;
@@ -22,6 +24,10 @@ public class PublicationService {
     public PublicationDTO save(PublicationDTO publicationDTO) {
         Publication publication = publicationRepository.save(publicationMapper.toEntity(publicationDTO));
         return publicationMapper.toDto(publication);
+    }
+
+    public List<PublicationDTO> getAll() {
+        return publicationMapper.toDto(publicationRepository.findAll());
     }
 
     public PublicationDTO findById(Long id) {
