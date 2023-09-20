fb125d110978c6494b15c3f1770c04fea67e5461
Manuel Gainza <gainza2014@gmail.com>
2023-09-20 18:24:21 -0300

    Add generic methods for list mapping

--- backend/src/main/java/com/crazyphone/CrazyPhone/services/mapper/EntityMapper.java
+++ backend/src/main/java/com/crazyphone/CrazyPhone/services/mapper/EntityMapper.java
@@ -1,7 +1,13 @@
 package com.crazyphone.CrazyPhone.services.mapper;
 
+import java.util.List;
+
 public interface EntityMapper<DTO, ENTITY> {
      DTO toDto(ENTITY entity);
 
      ENTITY toEntity(DTO dto);
+
+     List<DTO> toDto(List<ENTITY> entityList);
+
+     List<ENTITY> toEntity(List<DTO> dtoList);
 }
