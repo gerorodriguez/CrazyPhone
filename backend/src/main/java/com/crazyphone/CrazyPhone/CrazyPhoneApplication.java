6befbb20be0b446180fb3bed7917c2ac74ad99f6
Manuel Gainza <gainza2014@gmail.com>
2023-09-24 20:54:27 -0300

    generate tokens for jwt

--- backend/src/main/java/com/crazyphone/CrazyPhone/CrazyPhoneApplication.java
+++ backend/src/main/java/com/crazyphone/CrazyPhone/CrazyPhoneApplication.java
@@ -1,9 +1,12 @@
 package com.crazyphone.CrazyPhone;
 
+import com.crazyphone.CrazyPhone.config.rsaKeyProperties.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class CrazyPhoneApplication {
 
 	public static void main(String[] args) {
