ac087e2d82971fc25fa06e175adb50d0b6ef7fed
alvarodlp98 <110502302+alvarodlp98@users.noreply.github.com>
2023-11-06 21:33:25 -0300

    home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import ProductCard from '../../components/productCard/ProductCard'
+import ProductCard from '../../components/productCard/ProductCard';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Home = () => {
   return (
-   <>
-    <ProductCard />
-    </>
-  )
-}
+    <Container
+      fluid
+      className="d-flex justify-content-center align-items-center mb-2   "
+    >
+      <Row className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+        <Col>
+          <ProductCard className="mb-3" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
