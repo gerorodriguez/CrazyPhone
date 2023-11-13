ad0c0d571bd66f6c7d3972b1d813f153cd7bb34e
Manuel Gainza <gainza2014@gmail.com>
2023-11-13 20:26:10 -0300

    get current user login

--- backend/src/main/java/com/crazyphone/CrazyPhone/utils/SecurityUtils.java
+++ backend/src/main/java/com/crazyphone/CrazyPhone/utils/SecurityUtils.java
@@ -1,10 +1,15 @@
 package com.crazyphone.CrazyPhone.utils;
 
 import com.crazyphone.CrazyPhone.entities.Authority;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -14,5 +19,20 @@ public class SecurityUtils {
         return authorities.stream()
                 .map(authority -> new SimpleGrantedAuthority(authority.getName()))
                 .collect(Collectors.toList());
+    }
+
+    public static Optional<String> getCurrentUserLogin() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return Optional.ofNullable(extractPrincipal(securityContext.getAuthentication()));
+    }
+
+    private static String extractPrincipal(Authentication authentication) {
+        if (authentication == null) {
+            return null;
+        } else {
+            Jwt jwtToken = (Jwt) authentication.getPrincipal();
+            String username = jwtToken.getClaimAsString("sub");
+            return username;
+        }
     }
 }
