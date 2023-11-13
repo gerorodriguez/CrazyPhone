0dd1445d5cf65119c5d923163ca397e210d701f8
Manuel Gainza <gainza2014@gmail.com>
2023-11-13 20:27:55 -0300

    update auth controller

--- backend/src/main/java/com/crazyphone/CrazyPhone/controllers/AuthController.java
+++ backend/src/main/java/com/crazyphone/CrazyPhone/controllers/AuthController.java
@@ -1,15 +1,18 @@
 package com.crazyphone.CrazyPhone.controllers;
 
 import com.crazyphone.CrazyPhone.services.auth.TokenService;
+import com.crazyphone.CrazyPhone.services.dto.AuthenticationResponse;
 import com.crazyphone.CrazyPhone.services.dto.LoginDTO;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class AuthController {
 
     private final TokenService tokenService;
@@ -22,8 +25,9 @@ public class AuthController {
     }
 
     @PostMapping("/authenticate")
-    public String authenticate(@RequestBody LoginDTO loginDTO) {
+    public AuthenticationResponse authenticate(@RequestBody LoginDTO loginDTO) {
         Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.password()));
-        return tokenService.generateToken(auth);
+        String token = tokenService.generateToken(auth);
+        return new AuthenticationResponse(token);
     }
 }
