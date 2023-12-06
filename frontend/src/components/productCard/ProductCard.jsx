9ff48620b4929c43215e1891cd61bf2876bff3fd
manumrighi <manuelmorichi08@gmail.com>
2023-12-05 21:26:08 -0300

    update home page with fake data

--- frontend/src/components/productCard/ProductCard.jsx
+++ frontend/src/components/productCard/ProductCard.jsx
@@ -1,25 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function ProductCard() {
+function ProductCard({id, title, price, description}) {
+
   return (
-    <Card style={{ width: 'auto' }} className="rounded-3 mx-2 shadow">
+    <Card key={id} style={{ width: 'auto' }} className="rounded-3 mx-2 shadow">
       <Card.Img
         variant="top"
         src="https://cdn.dxomark.com/wp-content/uploads/medias/post-155689/Apple-iPhone-15-Pro-Max_-blue-titanium_featured-image-packshot-review.jpg"
       />
       <Card.Body>
         <Card.Title className="d-flex justify-content-center mb-3 title">
-          Iphone 15 Pro Max
+          {title}
         </Card.Title>
         <Card.Text className="d-flex justify-content-center align-items-center">
-          Some quick example text to build on the card title and make.
+          {description}
         </Card.Text>
         <Card.Text
           className="d-flex justify-content-between align-items-center price"
           style={{ fontSize: '19px', margin: '1% 0' }}
-        >
-          $230,00
+          >
+          {price} USD
           <Button className="ml-auto">Ver</Button>
         </Card.Text>
       </Card.Body>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,7 +5,77 @@ import { ThemeContext } from '../../contexts/theme/theme.context';
 import { useContext } from 'react';
 
 const Home = () => {
+
   const { theme } = useContext(ThemeContext);
+
+  const publicationsList = [
+    {
+      id: 1,
+      title: "Iphone 15 Pro Max",
+      price: 230,
+      storage: 128,
+      description: "Some quick example text to build on the card title and make.",
+      phoneNumber: "123456789",
+      instagram: "instagram",
+      state: "Rosario",
+    },
+    {
+      id: 2,
+      title: "Iphone 14 Pro Max",
+      price: 300,
+      storage: 128,
+      description: "Some quick example text to build on the card title and make.",
+      phoneNumber: "123456789",
+      instagram: "instagram",
+      state: "Santa Fe",
+    },
+    {
+      id: 3,
+      title: "Iphone 11 Pro Max",
+      price: 200,
+      storage: 64,
+      description: "Some quick example text to build on the card title and make.",
+      phoneNumber: "123456789",
+      instagram: "instagram",
+      state: "Galvez Fe",
+    },
+    {
+      id: 4,
+      title: "Iphone 10 Pro Max",
+      price: 100,
+      storage: 64,
+      description: "Some quick example text to build on the card title and make.",
+      phoneNumber: "12341234234",
+      instagram: "instagram",
+      state: "Santa Fe",
+    },
+    {
+      id: 5,
+      title: "Iphone 15 Pro Max",
+      price: 500,
+      storage: 128,
+      description: "Some quick example text to build on the card title and make.",
+      phoneNumber: "123456789",
+      instagram: "instagram",
+      state: "Santa Fe",
+    },
+  ];
+
+  const publicationsMapped = publicationsList.map((publication) => (
+    <Col key={publication.id}>
+      <ProductCard className="mb-3"
+        key={publication.id} 
+        title={publication.title}
+        price={publication.price}
+        storage={publication.storage}
+        description={publication.description}
+        phoneNumber={publication.phoneNumber}
+        instagram={publication.instagram}
+        state={publication.state}
+      />
+    </Col>
+  ));
+
   return (
     <Container
       data-bs-theme={theme}
@@ -14,33 +84,9 @@ const Home = () => {
     >
       <ToggleTheme />
       <Row className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
-        <Col>
-          <ProductCard className="mb-3" />
-        </Col>
+       
+          {publicationsMapped}
+        
       </Row>
     </Container>
   );
