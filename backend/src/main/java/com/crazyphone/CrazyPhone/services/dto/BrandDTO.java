3d3e698a4d5883391707910aa48e0f4a359adf29
manumrighi <manuelmorichi08@gmail.com>
2023-12-09 20:05:12 -0300

    Update BrandDTO and ModelDTO classes

--- backend/src/main/java/com/crazyphone/CrazyPhone/services/dto/BrandDTO.java
+++ backend/src/main/java/com/crazyphone/CrazyPhone/services/dto/BrandDTO.java
@@ -5,7 +5,7 @@ import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.List;
 
-    public record BrandDTO(Long id, @NotNull String brandName, List<Model> models) {
+    public record BrandDTO(Long id, @NotNull String brandName, List<ModelDTO> models) {
 
     }
 
