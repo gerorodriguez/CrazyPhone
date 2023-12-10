4e7dffd2a6dada1c00c2d1ca8d9c311086c915b0
gerorodriguez <110501756+gerorodriguez@users.noreply.github.com>
2023-12-10 00:34:15 -0300

    remove toggletheme

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import ProductCard from '../../components/productCard/ProductCard';
 import { Container, Row, Col } from 'react-bootstrap';
-import ToggleTheme from '../../components/toggleTheme/ToggleTheme';
 import { ThemeContext } from '../../contexts/theme/theme.context';
 import { useContext } from 'react';
 
@@ -12,7 +11,6 @@ const Home = () => {
       fluid
       className={`d-flex justify-content-center align-items-center mb-2 ${theme === 'dark' ? 'bg-dark' : 'bg-light'}`}
     >
-      <ToggleTheme />
       <Row className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         <Col>
           <ProductCard className="mb-3" />
