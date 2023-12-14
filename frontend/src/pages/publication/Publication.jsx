bbcb5188875e7ec601bcaeddba139cbb581dbee7
gerorodriguez <110501756+gerorodriguez@users.noreply.github.com>
2023-12-14 20:31:51 -0300

    fix

--- frontend/src/pages/publication/Publication.jsx
+++ frontend/src/pages/publication/Publication.jsx
@@ -192,7 +192,7 @@ const Publication = () => {
           await updatePublication(id, formValues);
           setSuccessMessage('Publicación actualizada con éxito.');
           navigate('/myPublications');
-        } else { 
+        } else {
           registerNewPublication(formValues);
           setSuccessMessage('Nueva publicación creada con éxito.');
           navigate('/');
@@ -211,18 +211,15 @@ const Publication = () => {
         newPublication,
         selectedImages,
       );
-      toggleLoading(false)
+      toggleLoading(false);
       console.log('Publication saved:', savedPublication);
     } catch (error) {
       setPublicationErrors(error.message);
-      toggleLoading(false)
+      toggleLoading(false);
     }
   };
 
   return (
-    <Form onSubmit={handleSubmit} className="p-3">
-      {!isLoading ? (
-      <>
     <Form
       onSubmit={handleSubmit}
       className={`p-3 ${
@@ -230,207 +227,231 @@ const Publication = () => {
       }`}
     >
       {!isLoading ? (
-      <>
-      <Row>
-        <Col md={6}>
-          <Form.Group controlId="title" className="mb-3">
-            <Form.Label>Título de la publicación</Form.Label>
-            <Form.Control
-              controlId="titulo"
-              type="text"
-              placeholder="Ingrese un título"
-              onChange={handleInputChange}
-              value={formValues.title}
-            />
-            <Form.Text className="text-danger">{formErrors.title}</Form.Text>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group controlId="price" className="mb-3">
-            <Form.Label>Precio</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Ingrese un precio"
-              onChange={handleInputChange}
-              value={formValues.price}
-            />
-            <Form.Text className="text-danger">{formErrors.price}</Form.Text>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form.Group controlId="brand" className="mb-3">
-            <Form.Label>Marca</Form.Label>
-            <Form.Select value={formValues.brand} onChange={handleInputChange}>
-              <option value="">Seleccione una marca</option>
-              <option value="Apple">Apple</option>
-              <option value="Samsung">Samsung</option>
-              <option value="Xiaomi">Xiaomi</option>
-              <option value="Oppo">Oppo</option>
-            </Form.Select>
-            <Form.Text className="text-danger">{formErrors.brand}</Form.Text>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group controlId="model" className="mb-3">
-            <Form.Label>Modelo</Form.Label>
-            <Form.Select
-              value={formValues.model}
-              onChange={handleInputChange}
-              disabled={!formValues.brand}
-            >
-              {availableModels[formValues.brand]?.map((model, index) => (
-                <option key={index} value={model}>
-                  {model}
-                </option>
-              ))}
-            </Form.Select>
-            <Form.Text className="text-danger">{formErrors.model}</Form.Text>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form.Group controlId="storage" className="mb-3">
-            <Form.Label>Capacidad</Form.Label>
-            <Form.Select
-              value={formValues.storage}
-              onChange={handleInputChange}
-            >
-              <option value="">Seleccione su capacidad</option>
-              <option value="64">64GB</option>
-              <option value="128">128GB</option>
-              <option value="256">256GB</option>
-            </Form.Select>
-            <Form.Text className="text-danger">{formErrors.storage}</Form.Text>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group controlId="instagramAccount" className="mb-3">
-            <Form.Label>Instagram</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="@instagram"
-              onChange={handleInputChange}
-              value={formValues.instagramAccount}
-            />
-            <Form.Text className="text-danger">
-              {formErrors.instagramAccount}
-            </Form.Text>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form.Group controlId="description" className="mb-3">
-            <Form.Label>Descripción</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Ingrese una descripción"
-              onChange={handleInputChange}
-              value={formValues.description}
-            />
-            <Form.Text className="text-danger">
-              {formErrors.description}
-            </Form.Text>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group controlId="phoneNumber" className="mb-3">
-            <Form.Label>Teléfono</Form.Label>
-            <Form.Control
-              type="tel"
-              placeholder="Ingrese un número de teléfono"
-              onChange={handleInputChange}
-              value={formValues.phoneNumber}
-            />
-            <Form.Text className="text-danger">
-              {formErrors.phoneNumber}
-            </Form.Text>
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <Form.Group
-            controlId="state"
-            className="mb-3 "
-            onChange={handleInputChange}
-          >
-            <Form.Label>Provincia</Form.Label>
-            <Form.Select value={formValues.state}>
-              <option value="">Seleccione una provincia</option>
-              <option value="Santa Fe">Santa Fe</option>
-              <option value="Buenos Aires">Buenos Aires</option>
-              <option value="Córdoba">Córdoba</option>
-              <option value="Mendoza">Mendoza</option>
-            </Form.Select>
-            <Form.Text className="text-danger">{formErrors.state}</Form.Text>
-          </Form.Group>
-        </Col>
-        <Col md={6}>
-          <Form.Group controlId="imagenes" className="mb-3">
-            <Form.Label>Imágenes</Form.Label>
-            <Row className="mb-2">
-              <Col md={6}>
+        <>
+          <Row>
+            <Col md={6}>
+              <Form.Group controlId="title" className="mb-3">
+                <Form.Label>Título de la publicación</Form.Label>
                 <Form.Control
-                  type="file"
-                  id="file"
-                  accept="image/png, image/jpeg"
-                  multiple
-                  onChange={handleFileChange}
+                  controlId="titulo"
+                  type="text"
+                  placeholder="Ingrese un título"
+                  onChange={handleInputChange}
+                  value={formValues.title}
                 />
-              </Col>
-            </Row>
-            {imagePreview.length > 0 && (
-              <Row>
-                {imagePreview.map((previewUrl, index) => (
-                  <Col key={index} md={3}>
-                    <div style={{ position: 'relative' }}>
-                      <img
-                        src={previewUrl}
-                        alt={`Preview ${index}`}
-                        style={{ width: '100%' }}
-                      />
-                      <Button
-                        variant="danger"
-                        size="sm"
-                        style={{
-                          position: 'absolute',
-                          top: '0',
-                          right: '0',
-                        }}
-                        onClick={() => handleImageRemove(index)}
-                      >
-                        X
-                      </Button>
-                    </div>
+                <Form.Text className="text-danger">
+                  {formErrors.title}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="price" className="mb-3">
+                <Form.Label>Precio</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Ingrese un precio"
+                  onChange={handleInputChange}
+                  value={formValues.price}
+                />
+                <Form.Text className="text-danger">
+                  {formErrors.price}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Form.Group controlId="brand" className="mb-3">
+                <Form.Label>Marca</Form.Label>
+                <Form.Select
+                  value={formValues.brand}
+                  onChange={handleInputChange}
+                >
+                  <option value="">Seleccione una marca</option>
+                  <option value="Apple">Apple</option>
+                  <option value="Samsung">Samsung</option>
+                  <option value="Xiaomi">Xiaomi</option>
+                  <option value="Oppo">Oppo</option>
+                </Form.Select>
+                <Form.Text className="text-danger">
+                  {formErrors.brand}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="model" className="mb-3">
+                <Form.Label>Modelo</Form.Label>
+                <Form.Select
+                  value={formValues.model}
+                  onChange={handleInputChange}
+                  disabled={!formValues.brand}
+                >
+                  {availableModels[formValues.brand]?.map((model, index) => (
+                    <option key={index} value={model}>
+                      {model}
+                    </option>
+                  ))}
+                </Form.Select>
+                <Form.Text className="text-danger">
+                  {formErrors.model}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Form.Group controlId="storage" className="mb-3">
+                <Form.Label>Capacidad</Form.Label>
+                <Form.Select
+                  value={formValues.storage}
+                  onChange={handleInputChange}
+                >
+                  <option value="">Seleccione su capacidad</option>
+                  <option value="64">64GB</option>
+                  <option value="128">128GB</option>
+                  <option value="256">256GB</option>
+                </Form.Select>
+                <Form.Text className="text-danger">
+                  {formErrors.storage}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="instagramAccount" className="mb-3">
+                <Form.Label>Instagram</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="@instagram"
+                  onChange={handleInputChange}
+                  value={formValues.instagramAccount}
+                />
+                <Form.Text className="text-danger">
+                  {formErrors.instagramAccount}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Form.Group controlId="description" className="mb-3">
+                <Form.Label>Descripción</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese una descripción"
+                  onChange={handleInputChange}
+                  value={formValues.description}
+                />
+                <Form.Text className="text-danger">
+                  {formErrors.description}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="phoneNumber" className="mb-3">
+                <Form.Label>Teléfono</Form.Label>
+                <Form.Control
+                  type="tel"
+                  placeholder="Ingrese un número de teléfono"
+                  onChange={handleInputChange}
+                  value={formValues.phoneNumber}
+                />
+                <Form.Text className="text-danger">
+                  {formErrors.phoneNumber}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Form.Group
+                controlId="state"
+                className="mb-3 "
+                onChange={handleInputChange}
+              >
+                <Form.Label>Provincia</Form.Label>
+                <Form.Select value={formValues.state}>
+                  <option value="">Seleccione una provincia</option>
+                  <option value="Santa Fe">Santa Fe</option>
+                  <option value="Buenos Aires">Buenos Aires</option>
+                  <option value="Córdoba">Córdoba</option>
+                  <option value="Mendoza">Mendoza</option>
+                </Form.Select>
+                <Form.Text className="text-danger">
+                  {formErrors.state}
+                </Form.Text>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group controlId="imagenes" className="mb-3">
+                <Form.Label>Imágenes</Form.Label>
+                <Row className="mb-2">
+                  <Col md={6}>
+                    <Form.Control
+                      type="file"
+                      id="file"
+                      accept="image/png, image/jpeg"
+                      multiple
+                      onChange={handleFileChange}
+                    />
                   </Col>
-                ))}
-              </Row>
-            )}
-          </Form.Group>
-        </Col>
-      </Row>
-      <Row className="justify-content-end mb-3">
-        <Col xs="auto">
-          <Button variant="danger" href="/" className="mx-3">
-            Cancelar
-          </Button>
-          <Button variant="success" type="submit">
-            {id ? 'Actualizar' : 'Publicar'}
-          </Button>
-        </Col>
-      </Row>
-      {successMessage && (
-        <Alert variant="success" className="mb-3">
-          {successMessage}
-        </Alert>
-      )}
-      </>
+                </Row>
+                {imagePreview.length > 0 && (
+                  <Row>
+                    {imagePreview.map((previewUrl, index) => (
+                      <Col key={index} md={3}>
+                        <div style={{ position: 'relative' }}>
+                          <img
+                            src={previewUrl}
+                            alt={`Preview ${index}`}
+                            style={{ width: '100%' }}
+                          />
+                          <Button
+                            variant="danger"
+                            size="sm"
+                            style={{
+                              position: 'absolute',
+                              top: '0',
+                              right: '0',
+                            }}
+                            onClick={() => handleImageRemove(index)}
+                          >
+                            X
+                          </Button>
+                        </div>
+                      </Col>
+                    ))}
+                  </Row>
+                )}
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="justify-content-end mb-3">
+            <Col xs="auto">
+              <Button variant="danger" href="/" className="mx-3">
+                Cancelar
+              </Button>
+              <Button variant="success" type="submit">
+                {id ? 'Actualizar' : 'Publicar'}
+              </Button>
+            </Col>
+          </Row>
+          {successMessage && (
+            <Alert variant="success" className="mb-3">
+              {successMessage}
+            </Alert>
+          )}
+        </>
       ) : (
-        <Spinner style={{position: 'absolute', left: 0, right: 0, bottom: 0, top: 0, margin: "auto"}}/>
+        <Spinner
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            margin: 'auto',
+          }}
+        />
       )}
     </Form>
   );
