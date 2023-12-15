976408e82d01c96fdae95578cac5018762d46df9
gerorodriguez <110501756+gerorodriguez@users.noreply.github.com>
2023-12-14 22:35:48 -0300

    fix layout

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,20 +1,26 @@
 import Header from '../header/Header.jsx';
-import {Outlet} from 'react-router-dom';
-import {useContext} from 'react';
-import {ThemeContext} from '../../contexts/theme/theme.context.jsx';
-import {APIContext} from "../../services/ApiContext.jsx";
+import { Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import { ThemeContext } from '../../contexts/theme/theme.context.jsx';
+import { APIContext } from '../../services/ApiContext.jsx';
 
 const Layout = () => {
-    const {theme} = useContext(ThemeContext);
-    const {isLoading} = useContext(APIContext);
+  const { theme } = useContext(ThemeContext);
+  const { isLoading } = useContext(APIContext);
 
-    return (
-        <div className={`${theme === 'dark' ? 'bg-dark' : 'bg-light'} ${isLoading && 'opacity-25'} `}>
-            <Header/>
-                <main><Outlet/></main>
-        </div>
-    );
-
+  return (
+    <div
+      style={{ flex: 1, height: '100vh' }}
+      className={`${theme === 'dark' ? 'bg-dark' : 'bg-light'} ${
+        isLoading && 'opacity-25'
+      } `}
+    >
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
